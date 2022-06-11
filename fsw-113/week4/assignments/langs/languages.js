8709bccd2e2be0779939788cb38d252be9634f35
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-06-10 20:00:13 -0400

    .

--- fsw-113/week4/assignments/langs/languages.js
+++ fsw-113/week4/assignments/langs/languages.js
@@ -4,14 +4,24 @@ const lang = 'JavaScript'
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
 
+document.getElementById("submit").addEventListener("click", function() {
+    let inputArray = [...document.querySelectorAll("input")];
+    chkLang(inputArray);
+});
 
 function chkLang(langs) {
     let result = false
+    langs.forEach(input => {
+        if (input.value === lang){
+            result = true;
+        }
+    });
 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
 
     let obj = document.querySelector('#TestResult')
+    
     if (result) 
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
--- fsw-113/week4/assignments/scavenger/scavenger.js
+++ fsw-113/week4/assignments/scavenger/scavenger.js
@@ -1,10 +1,20 @@
 // Create an event listenter for the 'submit' button that calls the combineLists() function
 
+document.querySelector("#submit").addEventListener("click", combineLists);
+
 // Modify the combineLists() function to create arrays from each scavenger hunt element in the scavenger.html document. 
 // Then use the spread operator to combine these arrays into a single array. 
 // Sort that array and display it in the 'AllItems' element on the scavenger.html document.
 
 function combineLists() {
+    var scav0 = document.getElementById('scavenger-0').value.split()
+    var scav1 = document.getElementById('scavenger-1').value.split()
+    var scav2 = document.getElementById('scavenger-2').value.split()
+    var scav3 = document.getElementById('scavenger-3').value.split()
+
+    var fullList = [...scav0, ...scav1, ...scav2, ...scav3]
+    fullList.sort()
+    document.querySelector('#AllItems').innerText = fullList.join();
 }
 
 // The following code is a bonus addition that facilitates data entry on the scavenger.html document.
@@ -12,21 +22,21 @@ function combineLists() {
 // list. The value of that will be added to the appropriate textarea along with any necessary commas.
 
 let objTargetText 
-const objUL = document.querySelector('#lstItems')
+const objUL = document.querySelector('#lstItems');
 
 objUL.onclick = function(event) {
-    var source = getEventTarget(event)
+    var source = getEventTarget(event);
     if (!objTargetText.innerHTML)
-        objTargetText.innerHTML = source.innerHTML
+        objTargetText.innerHTML = source.innerHTML;
     else
-        objTargetText.innerHTML += ',' + source.innerHTML
+        objTargetText.innerHTML += ',' + source.innerHTML;
 }
 
 function getEventTarget(e) {
     e = e || window.event
-    return e.target || e.srcElement
+    return e.target || e.srcElement;
 }
 
 function setTarget(obj) {
-    objTargetText = obj
+    objTargetText = obj;
 }
