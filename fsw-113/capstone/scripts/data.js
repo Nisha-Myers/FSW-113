8bb71412798572b1651be8280751ebbf2815f1cc
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-07-04 19:31:36 -0400

    .

--- fsw-113/capstone/scripts/data.js
+++ fsw-113/capstone/scripts/data.js
@@ -1,7 +1,7 @@
 // Call the "getSystems()" function in such a way that when the page loads, the "system" select element displays the three sytems 
 // whose parentID is zero.
-
-function getSystems() {
+let system = document.querySelector("#system")
+getSystems(0, system) 
     // This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
     // Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
     // After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
@@ -9,18 +9,46 @@ function getSystems() {
     // this new array and the DOM element to the "populateDD" function (below).
 
     // Note that although a number being passed as a parameter into a function may look like a numeral, it may be a string value 
-    // instead, and may need to be converted to an integer. 
-}
+    // instead, and may need to be converted to an integer.
+    let wait;
 
-function populateDD() {
+async function getSystems(parentID, system){
+    const getFetch = await fetch("./data.json")
+        let data = await getFetch.json()
+        populateDD(filterData, system)
+    }
+
+
+function populateDD(filterData, system) {
     // This function receives the array and DOM element from the "getSystems()" function (above). This function should fill the 
     // appropriate DOM element with options from which the user can select. However, since that DOM element has an "onChange" event, this
     // function first needs to add an option that says "Select an Item". Then use a looping mechanism (week one) to create the rest of  
     // the select element's options using the sysName and sysID from the passed in array (see week five).
+    system.innerHTML = " ";
+    system.options = [];
+    if(filterData.length !== 0){
+        var selectItem = document.createElement("option");
+        selectItem.textContent = "Select an Item";
+        system.appendChild(selectItem);
+
+    filterData.array.forEach(element => {
+        document.getElementById("subSystem").value = " ";
+        var optn = document.createElement = ("option")
+        optn.textContent = `${element.sysName}`;
+        optn.value = element.sysID;
+        optn.style.color = 'white'
+        system.appendChild(optn)
+    });
+    }
 }
 
-document.querySelector('#system').addEventListener(
+document.querySelector('#system').addEventListener("change", function(){
+    getSystems(this.value, document.querySelector("#subSystem"));
+})
+
+document.querySelector("#resetBtn").addEventListener("click", (e) => {
+    document.querySelector("#system").removeOptions();
+})
     // This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
     // to the "getSystems()" function along with the "subSystem" DOM element so that the second drop-down list is populated with the 
     // appropriate sub-systems from the data.json file.
-)
